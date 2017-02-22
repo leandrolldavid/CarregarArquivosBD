593764d5e9b43fa41bfa4ac79770a53be2920c4f
leandrolldavid <leandrolldavid@gmail.com>
2017-02-21 22:29:05 -0300

    carregar
    carregar arquivos do banco

---
+++ TesteHibernate/src/java/Produto.java
@@ -0,0 +1,60 @@
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ra21551055
+ */
+@Entity (name = "tbProduto")// torna a classe visivel ao hibernate 
+public class Produto {
+    
+    @Id
+    private int id;
+    @Column(name = "nome_prod")
+    private String nome;
+    private float preco;
+    private String fabricante;
+
+  
+    public String getNome() {
+        return nome;
+    }
+  
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+  
+    public float getPreco() {
+        return preco;
+    }
+  
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+   
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+     public int getId() {
+        return id;
+    }
+
+      public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+}
